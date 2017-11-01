bce74763fb98acc3bc5213c2b4241c889b8ed952
Nicolas Martin <nihco2@gmail.com>
2017-11-01 21:17:39 +0100

    Improve carousel

--- public/webapp/src/components/tuto.vue
+++ public/webapp/src/components/tuto.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="tuto pt flex">
-    <h3 v-if="firstSteps" class="jim">Fabriquez votre voile</h3>
+    <h3 v-if="firstSteps" class="jim">Fabriquez votre voile :</h3>
     <h3 v-else class="jim">Quelques Conseils...</h3>
-      <el-carousel :interval="4000" trigger="click" height="270px" indicator-position="outside" arrow="always" :autoplay="true" @change="changeSlide">
+      <el-carousel :interval="4000" trigger="click" height="300px" indicator-position="outside" arrow="always" :autoplay="true" @change="changeSlide">
       <el-carousel-item v-for="item in items" :key="item.url">
-        <img class="pic" :src="`${assetsUrl}${item.url}`">
         <p class="bitter sentence" v-html="item.sentence"></p>
+        <img class="pic" :src="`${assetsUrl}${item.url}`">
       </el-carousel-item>
     </el-carousel>
     <h3 class="jim loading" v-if="showStartButton" @click="go">GO !</h3>
