f9548481e725593663918640d440a93d0874b602
Kevin "Gaeel" Bradshaw-Rodriguez <gaeel@nanoleptic.net>
2017-12-20 18:19:08 +0100

    Added git-rev

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
+var git = require("git-rev");
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  git.tag(function(tag){
+    console.log(tag);
+    git.branch(function(branch){
+      console.log(branch);
+      git.short(function(short){
+        console.log(short);
+        res.render('index', { title: 'Paper Sail', git:{
+            tag: tag,
+            branch: branch,
+            short: short
+          }
+        });
+      });
+    });
+  });
 });
 
 module.exports = router;
